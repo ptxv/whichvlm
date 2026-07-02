5c47e7eaef3ecf3cb5c42665491dcd0a16d97705
2CTA <sethisrreyansh@gmail.com>
2026-07-02 15:45:32 -0700

    Add VRAM performance reserve (#24)

--- src/whichvlm/output/plan.py
+++ src/whichvlm/output/plan.py
@@ -26,7 +26,7 @@ from whichvlm.output import console
 from whichvlm.output.formatting import format_bytes, format_params
 
 PLAN_QUANTS = ("Q2_K", "Q3_K_M", "Q4_K_M", "Q5_K_M", "Q6_K", "Q8_0", "F16")
-PRACTICAL_PARTIAL_MAX_OFFLOAD_RATIO = 0.65
+PRACTICAL_PARTIAL_MAX_OFFLOAD_RATIO = 0.50
 PRACTICAL_PARTIAL_MIN_USABLE_VRAM_BYTES = 6 * BYTES_PER_GIB
 PRACTICAL_PARTIAL_MIN_TOK_PER_SEC = 2.0
 MULTI_GPU_SPEED_FACTOR = 0.70
